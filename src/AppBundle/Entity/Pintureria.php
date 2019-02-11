1297d484178dd2a47599dd32245b7d3595ead378
Adrian <adrian@MacBook-Air-de-Adrian.local>
2019-02-11 13:20:41 -0300

    add crud pintureria y tipos de trabajos

---
+++ src/AppBundle/Entity/Pintureria.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="pintureria")
+ * @Vich\Uploadable
+ */
+class Pintureria
+{
+        /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+  
+      /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $title;
+  
+     /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $description;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+ 
+ 
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+    // ...
+
+ 
+
+        public function getId()
+    {
+        return $this->id;
+    }
+  
+   public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+  
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+  
+  
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+    
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+    
+ 
+
+}
